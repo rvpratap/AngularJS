0666fc1bb7d843ff8d45ed4dd0fe517f7b5c4b61
Training_b6b.01.13 <Training_b6b.01.13@accenture.com>
2016-06-13 15:27:58 +0530

    Adding new chnages

--- AngualrJS/Ex4-CreateRead/app.js
+++ AngualrJS/Ex4-CreateRead/app.js
@@ -1,7 +1,8 @@
 angular.module('todoApp',[])
-.controller('todoCtrl', function($scope) {
+.controller('todoCtrl', function($scope, todoFactory) {
 	    var uid=0;
-		$scope.todos=[];
+		$scope.todos=todoFactory.get();
+
 		$scope.addToDo=function($event){
 			if($event.charCode === 13) {
 				var todo={
@@ -9,9 +10,38 @@ angular.module('todoApp',[])
 					text: $scope.todoText,
 					isCompleted: false
 				}
-				$scope.todos.push(todo);
-			}
-            
+				$scope.todos.push(todo);				
+			}	
+			//$scope.todoText='';            
 		}
 
-});
+		$scope.TodoEdit=function(todo){
+			todo.tmpvar=todo.text;
+			todo.todoShowHide=true;
+		}
+		$scope.TodoSave=function(todo){
+			todo.todoShowHide=false;			
+		}
+		$scope.TodoDelete=function($index){
+			$scope.todos.splice($index, 1);			
+		}
+		$scope.TodoCancel=function(todo){
+			todo.text=todo.tmpvar;
+			todo.todoShowHide=false;			
+		}
+})
+
+.controller('headerCtrl',function($scope, todoFactory) {
+	$scope.todos=todoFactory.get();
+
+})
+
+.factory('todoFactory', function() {
+	var todos=[];
+	return {
+		get: function() {
+			return todos;
+		}
+	};
+
+})
