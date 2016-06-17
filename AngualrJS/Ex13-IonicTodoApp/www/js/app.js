31025ae751399acea19e4ec31c05a3f7cd7d8038
Training_b6b.01.13 <Training_b6b.01.13@accenture.com>
2016-06-17 15:44:55 +0530

    17.05.2016 changes 4th

--- AngualrJS/Ex13-IonicTodoApp/www/js/app.js
+++ AngualrJS/Ex13-IonicTodoApp/www/js/app.js
@@ -74,14 +74,84 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 })
 
 
-.controller('todosCtrl', function($scope, todoRESTFactory) {
-
+.controller('todosCtrl', function($scope, todoRESTFactory, $ionicModal) {
+    $scope.data = {};
+    $scope.listCanSwipe = true;
     todoRESTFactory
-    .get()
-    .then(function(response){
-      $scope.todos=response.data;
-      console.log($scope.todos);
-    })
+        .get()
+        .then(function(response) {
+            $scope.todos = response.data;
+            console.log($scope.todos);
+        })
+
+    $ionicModal.fromTemplateUrl('templates/newtodo-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+    $scope.toggleNewTodoModal = function() {
+        $scope.data.todoText = '';
+        $scope.modal.show();
+    }
+
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+
+    $scope.createTodo = function() {
+        var todo = {
+            id: (Math.floor(Math.random() * 100)),
+            text: $scope.data.todoText
+        };
+        todoRESTFactory
+            .post(todo)
+            .then(function(response) {
+                if(response.status === 201) {
+                    $scope.todos.push(response.data);
+                    $scope.closeModal();
+                }
+            })
+    }
+
+    $scope.editTodo=function(todo){
+      $scope.editingTodoId=todo.id;
+      $scope.data.todoText=todo.text;
+      $scope.modal.show();
+    }
+
+    $scope.updateTodo=function(){
+      var updatedtodo={
+        id: $scope.editingTodoId,
+        text:$scope.data.todoText 
+      };
+      todoRESTFactory
+            .put(updatedtodo.id, updatedtodo)
+            .then(function(response) {
+                if(response.status === 200) {
+                    for (var i = 0; i < $scope.todos.length; i++) {
+                      if($scope.todos[i].id === updatedtodo.id){
+                          $scope.todos[i].text=updatedtodo.id;
+                      }
+                    }
+                }
+            })
+    }
+
+    $scope.toggleRemove=function(){
+      $scope.shouldShowDelete=true;
+
+    }
+    $scope.removeTodo=function(todo, $index){
+       todoRESTFactory
+            .delete(todo.id)
+            .then(function(response) {
+                if(response.status === 200) {
+                   $scope.todos.splice($index, 1);
+                   $scope.shouldShowDelete=false;
+                }
+            })
+    }
 
 })
 
